2d5a52fb98b0f890c11b06ec98fa046afb65e4b7
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2024-01-30 18:39:40 -0400

    chore: add service and module from prisma

--- proj-manag-api/src/app.module.ts
+++ proj-manag-api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectModule } from './project/project.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ProjectModule],
+  imports: [ProjectModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
