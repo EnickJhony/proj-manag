86f86e80edb812f838d40b328cb532384b6f5916
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2024-01-31 16:11:03 -0400

    chore: creating logic to create projects

--- proj-manag-api/src/project/project.controller.ts
+++ proj-manag-api/src/project/project.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { ProjectService } from './project.service';
 
@@ -6,8 +6,23 @@ import { ProjectService } from './project.service';
 export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
 
+  @Get()
+  findAll() {
+    return this.projectService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.projectService.findOne(id);
+  }
+
   @Post()
   create(@Body() createProjectDto: CreateProjectDto) {
     return this.projectService.create(createProjectDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.projectService.delete(id);
   }
 }
--- proj-manag-api/src/project/project.service.ts
+++ proj-manag-api/src/project/project.service.ts
@@ -1,17 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { response } from 'express';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 
 @Injectable()
 export class ProjectService {
-  create(createProjectDto: CreateProjectDto) {
-    return 'This action adds a new project';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createProjectDto: CreateProjectDto) {
+    const data: any = {
+      ...createProjectDto,
+    };
+    const createProject = await this.prisma.project.create({ data });
+    return createProject;
   }
 
-  findAll() {
-    return `This action returns all project`;
+  async findAll() {
+    const projects = await this.prisma.project.findMany({
+      include: {
+        members: true,
+      },
+    });
+
+    return projects;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} project`;
+  async findOne(id: number) {
+    const projectUnique = await this.prisma.project.findUnique({
+      where: { id: Number(id) },
+      include: {
+        members: true,
+      },
+    });
+    return projectUnique;
+  }
+
+  async delete(id: number) {
+    await this.prisma.project.delete({
+      where: { id: Number(id) },
+    });
+    return response.status(HttpStatus.OK);
   }
 }
